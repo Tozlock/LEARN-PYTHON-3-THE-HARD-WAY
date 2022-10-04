931c44bcc3cae58ec80d60192d4daf9eb95fdc84
Tozlock <arch.shady.mahdy@gmail.com>
2022-10-05 01:48:48 +0200

    EX24 : More Practice

--- EX23.py
+++ EX23.py
@@ -1,39 +1,51 @@
-# Strings, Bytes, and Character Encodings
+# # Strings, Bytes, and Character Encodings
 
-# 1. How modern computers store human languages for display and processing
-# and how Python3 calls this strings
-# 2. How you must “encode” and “decode” Python’s strings into a type called bytes
-# 3. How to handle errors in your string and byte handling
-# 4. How to read code and find out what it means even if you’ve never seen it before
+# # 1. How modern computers store human languages for display and processing
+# # and how Python3 calls this strings
+# # 2. How you must “encode” and “decode” Python’s strings into a type called bytes
+# # 3. How to handle errors in your string and byte handling
+# # 4. How to read code and find out what it means even if you’ve never seen it before
 
-import sys
-script, input_encoding, error = sys.argv
-
-
-def main(language_file, encoding, errors):
-    line = language_file.readline()
-    if line:
-        print_line(line, encoding, errors)
-        return main(language_file, encoding, errors)
+# import sys
+# script, input_encoding, error = sys.argv
 
 
-def print_line(line, encoding, errors):
-    next_lang = line.strip()
-    raw_bytes = next_lang.encode(encoding, errors=errors)
-    cooked_string = raw_bytes.decode(encoding, errors=errors)
+# def main(language_file, encoding, errors):
+#     line = language_file.readline()
+#     if line:
+#         print_line(line, encoding, errors)
+#         return main(language_file, encoding, errors)
 
-    print(raw_bytes, "<=====>", cooked_string)
+
+# def print_line(line, encoding, errors):
+#     next_lang = line.strip()
+#     raw_bytes = next_lang.encode(encoding, errors=errors)
+#     cooked_string = raw_bytes.decode(encoding, errors=errors)
+
+#     print(raw_bytes, "<=====>", cooked_string)
 
 
-languages = open("languages.txt", encoding="utf-8")
+# languages = open("languages.txt", encoding="utf-8")
 
-main(languages, input_encoding, error)
+# main(languages, input_encoding, error)
+a = [1, 2, 3]
 
-raw_bytes = b'\xd8\xa7\xd9\x84\xd8\xb9\xd8\xb1\xd8\xa8\xd9\x8a'
-utf_string = "العربية"
 
-print(raw_bytes.decode())
-print(utf_string.encode())
+def func():
+    b = [4, 5, 6]
+    print(a)
 
-print(raw_bytes.decode() == utf_string)
-print(utf_string.encode() == raw_bytes)
+
+func()
+print(a)
+
+a = [1, 2, 3]
+
+
+def func():
+    a[:] = [4, 5, 6]
+    print(a)
+
+
+func()
+print(a)
