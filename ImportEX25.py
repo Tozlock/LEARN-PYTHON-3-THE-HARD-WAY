c2131dcd666ae818bb1fc74f9dd82c308495de66
Tozlock <arch.shady.mahdy@gmail.com>
2022-10-16 03:37:13 +0200

    EX25: Even More Practice - Import

---
+++ ImportEX25.py
@@ -0,0 +1,48 @@
+import EX25 as ex25
+sentence = "hello, all good things come to those who wait."
+words = ex25.break_words(sentence)
+
+print("The sentence's words:")
+print(words)
+print()
+
+sorted_words = ex25.sort_words(words)
+print("The sorted words:")
+print(sorted_words)
+print()
+
+print("The first word:")
+ex25.print_first_word(words)
+print()
+
+print("The last word:")
+ex25.print_last_word(words)
+print()
+
+print("The words without the first and last words:")
+print(words)
+print()
+
+print("The first sorted word:")
+ex25.print_first_word(sorted_words)
+print()
+
+print("The last sorted word:")
+ex25.print_last_word(sorted_words)
+print()
+
+print("The sorted words without the first and last words:")
+print(sorted_words)
+print()
+
+print("The sorted sentence:")
+sorted_words = ex25.sort_sentence(sentence)
+print(sorted_words)
+print()
+
+print("The first and last words:")
+ex25.print_first_last(sentence)
+print()
+
+print("The first and last sorted words:")
+ex25.print_first_last_sorted(sentence)
